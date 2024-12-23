063c96979f9f633855c0e16a526a231a130a5a37
Huy Tran <huy.tran@lexer.io>
2024-12-23 12:47:51 +1100

    change code

--- auto-pr.py
+++ auto-pr.py
@@ -1,14 +1,15 @@
-import git
-import datetime
 import os
 from time import *
 from os import path
 from git import Repo
+from git_credentials import GIT_TOKEN
+import requests
+import json
 
 DIR_PATH = "/Users/huy.tran/Documents/personal_repos/auto-git-pr"
 REPO_REMOTE_PATH = "https://github.com/dhuy237/auto-git-pr.git"
 DEV_BRANCH = "dev"
-
+REPO_URL = "https://api.github.com/repos/dhuy237/auto-git-pr/pulls"
 
 def commit_files():
     repo = Repo(DIR_PATH)
@@ -35,4 +36,38 @@ def commit_files():
         else:
             print('no changes')
 
-commit_files()
+# commit_files()
+
+def create_pull_request(
+    title,
+    body,
+    base="main",
+    head=DEV_BRANCH,
+    token=GIT_TOKEN,
+):
+    headers = {"Authorization": "Bearer %s" % token,
+            'Content-Type': 'application/vnd.github+json'}
+    data = {
+            "title": title,
+            "base": base,
+            "head": head
+    }
+    if body:
+        data["body"] = body
+    
+    response = requests.post(
+        REPO_URL,
+        headers=headers,
+        data=json.dumps(data),
+        verify=False
+    )
+    if response.status_code not in range(200, 299):
+        raise Exception("Response %d: %s" % (response.status_code,
+                                             response.content))
+    return response.json()
+
+response = create_pull_request(
+    title="Test Auto-PR",
+    data="This is an automatically created PR."
+)
+print(response)
